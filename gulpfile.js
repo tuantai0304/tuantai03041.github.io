8f6e0bfd9dc5bd8b25b86d330a7b8a80fb8768bf
tuantai0304 <tuantai0304@gmail.com>
2017-09-19 14:42:42 +1000

    Finished review pay

--- gulpfile.js
+++ gulpfile.js
@@ -70,8 +70,8 @@ gulp.task('images', function () {
  * Watch html/md files, run jekyll & reload BrowserSync
  */
 gulp.task('watch', function () {
-    gulp.watch(['_scss/*/*.scss', '_scss/*.scss' ], ['sass']);
-    gulp.watch(['*.html', '_layouts/*.html','_includes/*/*.html', '_posts/*'], ['jekyll-rebuild']);
+    gulp.watch(['_scss/*/*.scss', '_scss/*.scss','_scss/*/*/*.scss' ], ['sass']);
+    gulp.watch(['*.html', '_layouts/*.html','_includes/*/*.html','_includes/*/*/*.html', '_posts/*'], ['jekyll-rebuild']);
 });
 
 /**
